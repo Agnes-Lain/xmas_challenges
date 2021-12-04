b0db7ec8720ae5a5af43b354f9cba469baa9f5e3
Yizhu ZONG <agnes.lain@gmail.com>
2021-12-04 16:43:18 +0100

    updated solution for day4, with delete method to reduce

--- xmas_day4.rb
+++ xmas_day4.rb
@@ -28,57 +28,65 @@ def check_column?(matrix)
   check_row?(matrix.transpose)
 end
 
-# part 1
+# part 1 + 2 (inspired by Pierre to delete the win boards, much better now)
 check_result = false
 new_data = data
 check_a.each do |num|
   break if check_result == true
   new_data = new_data.map{|matrix| mark_bingo(matrix, num)}
   new_data.each do |matrix|
-    break if check_result == true
+    # break if check_result == true
       # binding.pry
     if check_row?(matrix) || check_column?(matrix)
-      sum = matrix.flatten.select {|num| num != "x"}.sum
-      p sum * num
-      check_result = true
+      if new_data.size == data.size
+        sum = matrix.flatten.select {|num| num != "x"}.sum
+        p sum * num
+        new_data.delete(matrix)
+      elsif new_data.size == 1
+        sum = matrix.flatten.select {|num| num != "x"}.sum
+        p sum * num
+        check_result = true
+      else
+        new_data.delete(matrix)
+      end
     end
   end
 end
 
 # part 2
-def check_all_matrix(arr)
-  arr = arr.map do |matrix|
-    if check_row?(matrix) || check_column?(matrix)
-      matrix = true
-    else
-      matrix = false
-    end
-  end
-  arr
-end
+# def check_all_matrix(arr)
+#   arr = arr.map do |matrix|
+#     if check_row?(matrix) || check_column?(matrix)
+#       matrix = true
+#     else
+#       matrix = false
+#     end
+#   end
+#   arr
+# end
 
-def result?(arr)
-  arr = check_all_matrix(arr)
-  arr.count(true) == arr.size - 1
-end
+# def result?(arr)
+#   arr = check_all_matrix(arr)
+#   arr.count(true) == arr.size - 1
+# end
 
-check_result = false
-new_data = data
-index = nil
-check_a.each do |num|
-  break if check_result == true
-  new_data = new_data.map{|matrix| mark_bingo(matrix, num)}
-  if result?(new_data)
-    array = check_all_matrix(new_data)
-    index = array.index(false)
-  end
-  # binding.pry
-  if index != nil
-    if check_row?(new_data[index]) || check_column?(new_data[index])
-      check_result = true
-      final_data = new_data[index].flatten
-      final_data.delete(num)
-      p final_data.select {|num| num != "x"}.sum * num
-    end
-  end
-end
+# check_result = false
+# new_data = data
+# index = nil
+# check_a.each do |num|
+#   break if check_result == true
+#   new_data = new_data.map{|matrix| mark_bingo(matrix, num)}
+#   if result?(new_data)
+#     array = check_all_matrix(new_data)
+#     index = array.index(false)
+#   end
+#   # binding.pry
+#   if index != nil
+#     if check_row?(new_data[index]) || check_column?(new_data[index])
+#       check_result = true
+#       final_data = new_data[index].flatten
+#       final_data.delete(num)
+#       p final_data.select {|num| num != "x"}.sum * num
+#     end
+#   end
+# end
